d15c4a85afa4b45306df0f5e841279265acd9eb2
Ariana <a20210740@pucp.edu.pe>
2025-04-11 20:42:55 -0500

    incio

---
+++ Lab2_20210740/src/test/java/com/example/lab2_20210740/Lab220210740ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20210740;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab220210740ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
